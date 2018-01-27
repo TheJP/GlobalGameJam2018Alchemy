831071f19a36118ab4d20f59e47f5171cc552bab
Manuel Serquet <manuelserquet@hotmail.com>
2018-01-27 02:37:49 +0100

    Added first version of movement

---
+++ GlobalGameJam2018Alchemy/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 6.0F;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveDirection = Vector3.zero;
+        CharacterController controller = GetComponent<CharacterController>();
+        SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        if(moveDirection.sqrMagnitude > 0)
+        {
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= Speed;
+            controller.Move(moveDirection * Time.deltaTime);
+
+            int lookingDir = Mathf.RoundToInt(Vector3.SignedAngle(Vector3.down, moveDirection, Vector3.forward) / 90);
+            renderer.transform.localRotation = Quaternion.AngleAxis(lookingDir * 90, Vector3.forward);
+        }
+    }
+}
