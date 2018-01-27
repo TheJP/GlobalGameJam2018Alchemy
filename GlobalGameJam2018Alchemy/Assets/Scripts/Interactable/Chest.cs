9e414ecc6897dd76082b0bc1c00a28f13ee623cf
c4tTi <groux@gmx.net>
2018-01-27 14:45:24 +0100

    chest implemented produced item implemented

---
+++ GlobalGameJam2018Alchemy/Assets/Scripts/Interactable/Chest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GlobalGameJam2018Networking;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, IInteractable{
+
+    List<IItem> ItemsStored;
+    private int Places = 2;
+    private int Used = 0;
+
+    public bool CanInteract(IItem item)
+    {
+        if (item == null)
+        {
+            return Places > Used;
+        }
+        else
+        {
+            return Used > 0;
+        }
+    }
+
+    public IItem GetItem()
+    {
+        if (CanInteract(null)) {
+            IItem tempItem = ItemsStored[0];
+            ItemsStored.RemoveAt(0);
+            return tempItem;
+        }
+        return null;
+    }
+
+    public bool PutItem(IItem item)
+    {
+        if (CanInteract(item)) {
+            ItemsStored.Add(item);
+            return true;
+        }
+        return false;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
