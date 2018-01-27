21c3e0e7e942fe6af58d7e30bd5d62ca9c0b72a5
c4tTi <groux@gmx.net>
2018-01-27 02:04:37 +0100

    hey i did something

---
+++ GlobalGameJam2018Alchemy/Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    enum Type { powder, herb, hotLiquid, coldLiquid, mana, steam, paste};
+
+    // by element the color of the type is meant.
+    enum Element { blue, green, yellow, orange, red, violet}
+
+    private Type myType;
+    private Element myElement;
+    private int quantity = 1;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ GlobalGameJam2018Alchemy/Assets/Scripts/Workbench.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workbench : MonoBehaviour, IInteractable
+{
+
+
+    public bool canInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool putItem()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Item useWithoutItem()
+    {
+        throw new System.NotImplementedException();
+    }
+}
