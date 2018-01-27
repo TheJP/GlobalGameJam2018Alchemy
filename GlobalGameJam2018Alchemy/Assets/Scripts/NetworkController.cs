54f63094b53a2897fd0ef240fd9659e02894887c
TheJP <jp_55555@hotmail.com>
2018-01-27 12:23:08 +0100

    Created layout controller that can place pipes in the scene

--- GlobalGameJam2018Alchemy/Assets/Scripts/NetworkController.cs
+++ GlobalGameJam2018Alchemy/Assets/Scripts/NetworkController.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using GlobalGameJam2018Networking;
 using System;
 
 public class NetworkController : MonoBehaviour
 {
+    public LayoutController layout;
+
     private readonly Queue<Action> invokes = new Queue<Action>();
     private AlchemyNetwork Network { get; }
+    public LevelConfig Level { get; private set; } = null;
     public string PlumberName { get; private set; } = null;
     private bool Connected => PlumberName != null;
 
@@ -17,6 +19,26 @@ public class NetworkController : MonoBehaviour
         Network = new AlchemyNetwork(addInvoke);
         Network.Connected += plumberName => PlumberName = plumberName;;
         Network.ServerStopped += () => PlumberName = null;
+        Network.LevelStarted += levelConfig => {
+            Level = levelConfig;
+            layout.CreatePipes(levelConfig);
+        };
+    }
+
+    /// <summary>Method that is called if the user enters username and hostname [and port] in the gui and clicks on connect.</summary>
+    /// <param name="username">Username of the Alchemist.</param>
+    /// <param name="hostname">Hostname of the Plumbers Machine.</param>
+    /// <param name="port">Port on which the Plumbers game is runnning.</param>
+    public void Connect(string username, string hostname, int port = NetworkBase.DefaultPort)
+    {
+        Network.Connect(username, hostname, port);
+    }
+
+    /// <summary>Method that is called if the user clicks on single player.</summary>
+    public void PlaySinglePlayer()
+    {
+        // TODO: Create random pipe layout
+        // layout.CreatePipeLayout
     }
 
     private void Update()
