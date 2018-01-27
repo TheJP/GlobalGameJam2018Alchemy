584f72795b2bd6f879d7d5361f9b81e7e02f2e3c
Manuel Serquet <manuelserquet@hotmail.com>
2018-01-27 03:38:00 +0100

    Improved controlling of player: 	- Added looking Direction 	- Added Interaction test

--- GlobalGameJam2018Alchemy/Assets/PlayerMovement.cs
+++ GlobalGameJam2018Alchemy/Assets/PlayerMovement.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 6.0F;
+    public Sprite[] directions;
 
-	// Use this for initialization
-	void Start ()
+    public GameObject test;
+
+    private int lookingDir = 0;
+
+    private const float EPSILON = 1e-3f;
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
@@ -15,20 +23,62 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        Move();
+        HandleActions();
+    }
+
+    private void Move()
+    {
         Vector3 moveDirection = Vector3.zero;
         CharacterController controller = GetComponent<CharacterController>();
         SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
 
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-        if(moveDirection.sqrMagnitude > 0)
+        if (moveDirection.sqrMagnitude > EPSILON)
         {
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= Speed;
             controller.Move(moveDirection * Time.deltaTime);
 
-            int lookingDir = Mathf.RoundToInt(Vector3.SignedAngle(Vector3.down, moveDirection, Vector3.forward) / 90);
-            renderer.transform.localRotation = Quaternion.AngleAxis(lookingDir * 90, Vector3.forward);
+            lookingDir = Mathf.RoundToInt(Vector3.SignedAngle(Vector3.down, moveDirection, Vector3.forward) / 90);
+            //renderer.transform.localRotation = Quaternion.AngleAxis(lookingDir * 90, Vector3.forward);
+
+            if(directions.Length >= 4)
+            {
+                renderer.sprite = directions[lookingDir + 1];
+            }
+        }
+    }
+
+    private void HandleActions()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            // ToDo: temporary code to simulate interaction
+            Vector3 offset;
+            switch(lookingDir)
+            {
+                case -1:
+                    offset = Vector3.left;
+                    break;
+                case 0:
+                    offset = Vector3.down;
+                    break;
+                case 1:
+                    offset = Vector3.right;
+                    break;
+                case 2:
+                    offset = Vector3.up;
+                    break;
+                default:
+                    offset = Vector3.zero;
+                    Debug.Log("lookingDir: " + lookingDir);
+                    break;
+            }
+            offset *= 1.5f;
+
+            Instantiate(test, transform.position + offset, Quaternion.identity);
         }
     }
 }
