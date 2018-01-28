dc21bd39fa2881616f69d0da761a3d3d167d22c4
c4tTi <groux@gmx.net>
2018-01-28 11:19:41 +0100

    thrash scriipt

---
+++ GlobalGameJam2018Alchemy/Assets/Scripts/Interactable/Trash.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GlobalGameJam2018Networking;
+using UnityEngine;
+
+public class Thrash : MonoBehaviour, IInteractable
+{
+
+    List<IItem> ItemsStored;
+    private int Places = 2;
+    private int Used = 0;
+
+    public bool CanInteract(IItem item)
+    {
+        return item != null;
+        
+    }
+
+    public IItem GetItem()
+    {
+        return null;
+    }
+
+    public bool PutItem(IItem item)
+    {
+        if (CanInteract(item))
+        {
+            //remove item
+            return true;
+        }
+        return false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
