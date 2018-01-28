f4bca611a0e1c8a28bba4cc34a41bdae655ca3cb
Manuel Serquet <manuelserquet@hotmail.com>
2018-01-28 11:42:44 +0100

    Improved particle System of mortar

--- GlobalGameJam2018Alchemy/Assets/Scripts/PlayerTestController.cs
+++ GlobalGameJam2018Alchemy/Assets/Scripts/PlayerTestController.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.ItemSignatures;
 using GlobalGameJam2018Networking;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,13 +27,18 @@ public class PlayerTestController : PrefabLibraryBase
         Input.AddItem(new Ingredient(ItemType.Herb, IngredientColour.Green));
 
         var recipeHack = new List<Recipe> {
-            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Green) },
-            () => new Ingredient(ItemType.Herb, IngredientColour.Green),
-            5)};
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Black) }, () => new Ingredient(ItemType.Herb, IngredientColour.Blue), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Blue) }, () => new Ingredient(ItemType.Herb, IngredientColour.Green), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Green) }, () => new Ingredient(ItemType.Herb, IngredientColour.Orange), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Orange) }, () => new Ingredient(ItemType.Herb, IngredientColour.Red), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Red) }, () => new Ingredient(ItemType.Herb, IngredientColour.Violet), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Violet) }, () => new Ingredient(ItemType.Herb, IngredientColour.Yellow), 5),
+            new Recipe(new List<ItemSignature> { new IngredientSignature(ItemType.Herb, IngredientColour.Yellow) }, () => new Ingredient(ItemType.Herb, IngredientColour.Black), 5),};
 
         for(int i = 0; i < Things.Length; i++)
         {
             Things[i].MyRecipes = recipeHack;
+            Things[i].Efficiency = i + 1;
         }
 	}
 }
