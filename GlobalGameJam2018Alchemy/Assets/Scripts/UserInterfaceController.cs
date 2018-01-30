924f60f5c13df58432dac30be17393102fd20335
TheJP <jp_55555@hotmail.com>
2018-01-30 21:40:23 +0100

    Improved menu

--- GlobalGameJam2018Alchemy/Assets/Scripts/UserInterfaceController.cs
+++ GlobalGameJam2018Alchemy/Assets/Scripts/UserInterfaceController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(NetworkController))]
@@ -17,43 +18,60 @@ public class UserInterfaceController : MonoBehaviour
     public Text infoText;
 
     public Image background;
-    public CanvasScaler canvas;
+    public Canvas mainMenu;
+    public Canvas gameMenu;
+
     private float target = 1f;
     private float startTime;
+    private CanvasGroup mainMenuGroup;
 
     private void Start()
     {
+        mainMenuGroup = mainMenu?.GetComponent<CanvasGroup>();
+        Assert.IsNotNull(mainMenuGroup);
+
         int _;
         portInput.onValidateInput += (input, characterIndex, addedChar) => int.TryParse(addedChar.ToString(), out _) ? addedChar : '\0';
         usernameInput.onValidateInput += (input, characterIndex, addedChar) => char.IsLetterOrDigit(addedChar) ? addedChar :'\0';
 
-        GetComponent<NetworkController>().ServerConnected += HideUi;
-        GetComponent<NetworkController>().ServerStopped += ShowUi;
+        GetComponent<NetworkController>().ServerConnected += SwitchToIngameMenu;
+        GetComponent<NetworkController>().ServerStopped += SwitchToMainMenu;
 
         // Show UI if the player went game over and was playing single player
         GetComponent<GameController>().GameOver += success => {
-            if (!GetComponent<NetworkController>().Connected) { ShowUi(); }
+            if (!GetComponent<NetworkController>().Connected) { SwitchToMainMenu(); }
         };
 
         if (onlySinglePlayer) { ClickedSinglePlayer(); startTime = -10f; }
     }
 
-    private void HideUi() { target = 0f; startTime = Time.time; }
-    private void ShowUi() { target = 1f; startTime = Time.time; }
+    private void SwitchToIngameMenu()
+    {
+        target = 0f;
+        startTime = Time.time;
+        mainMenuGroup.interactable = false;
+        gameMenu.gameObject.SetActive(true);
+    }
+    private void SwitchToMainMenu()
+    {
+        target = 1f;
+        startTime = Time.time;
+        mainMenuGroup.interactable = true;
+        gameMenu.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
         // Animation transition in and transition out
         var interpolated = Mathf.Lerp(1f - target, target, Time.time - startTime);
-        background.color = new Color(background.color.r, background.color.g, background.color.b, interpolated);
-        canvas.scaleFactor = interpolated;
-        if (canvas.gameObject.activeInHierarchy && interpolated <= 0f) { canvas.gameObject.SetActive(false); }
-        if (!canvas.gameObject.activeInHierarchy && interpolated > 0f) { canvas.gameObject.SetActive(true); }
+        mainMenuGroup.alpha = interpolated;
+        if (mainMenu.enabled && interpolated <= 0f) { mainMenu.enabled = false; }
+        if (!mainMenu.enabled && interpolated > 0f) { mainMenu.enabled = true; }
     }
 
     public void ClickedSinglePlayer()
     {
-        HideUi();
+        SwitchToIngameMenu();
         GetComponent<NetworkController>().PlaySinglePlayer();
     }
 
@@ -78,6 +96,8 @@ public class UserInterfaceController : MonoBehaviour
         }
 
     }
+
+    public void ClickedQuitToMenu() => SwitchToMainMenu();
 
     private void ShowWarning(string warning) => StartCoroutine(DisplayWarning(warning));
 
