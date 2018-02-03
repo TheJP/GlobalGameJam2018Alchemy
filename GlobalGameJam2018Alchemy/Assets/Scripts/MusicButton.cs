0a99e82f3ac3e4b2e58ba838f671a0db1502c2a4
TheJP <jp_55555@hotmail.com>
2018-02-03 16:40:29 +0100

    Added background music and mute button

---
+++ GlobalGameJam2018Alchemy/Assets/Scripts/MusicButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicButton : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnMouseEnter() => animator.SetBool("Hovering", true);
+    private void OnMouseExit() => animator.SetBool("Hovering", false);
+    private void OnMouseUpAsButton() => AudioListener.pause = !AudioListener.pause;
+}
