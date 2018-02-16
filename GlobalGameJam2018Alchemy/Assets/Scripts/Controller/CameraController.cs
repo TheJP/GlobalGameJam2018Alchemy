b1f9df900466dba8fd748a734b2d3b533cf22d1e
TheJP <jp_55555@hotmail.com>
2018-02-16 16:13:49 +0100

    Restricted camera movement

--- GlobalGameJam2018Alchemy/Assets/Scripts/Controller/CameraController.cs
+++ GlobalGameJam2018Alchemy/Assets/Scripts/Controller/CameraController.cs
@@ -4,7 +4,11 @@
 public class CameraController : MonoBehaviour
 {
     private const int CameraMouseButton = 2;
+
     public Camera controlledCamera;
+
+    [Tooltip("Maximal allowed distance from original position in any direction")]
+    public float maxCameraMovement = 50f;
 
     private Vector3 origin;
     private bool active = false;
@@ -16,9 +20,14 @@ public class CameraController : MonoBehaviour
     {
         origin = transform.position;
         var game = GetComponent<GameController>();
-        game.GameOver += _ => active = false;
+        game.GameOver += _ =>
+        {
+            Cursor.lockState = CursorLockMode.None;
+            active = false;
+        };
         game.LevelStarted += () =>
         {
+            Cursor.lockState = CursorLockMode.Confined;
             transform.position = origin;
             active = true;
         };
@@ -29,17 +38,27 @@ public class CameraController : MonoBehaviour
         if (!active) { return; }
         if (Input.GetMouseButtonDown(CameraMouseButton))
         {
+            // Start camera movement
             target = IntersectGround();
             tracking = true;
         }
+        // Stop camera movement
         if (Input.GetMouseButtonUp(CameraMouseButton)) { tracking = false; }
         if (tracking)
         {
+            // Move camera
             var newTarget = IntersectGround();
             controlledCamera.transform.position += (target - newTarget);
         }
+
+        // Restrict camera movement
+        var position = controlledCamera.transform.position;
+        position.x = Mathf.Clamp(position.x, origin.x - maxCameraMovement, origin.x + maxCameraMovement);
+        position.z = Mathf.Clamp(position.z, origin.z - maxCameraMovement, origin.z + maxCameraMovement);
+        controlledCamera.transform.position = position;
     }
 
+    /// <summary>Finds the point on the xz-plane that the pointer is hovering over.</summary>
     private Vector3 IntersectGround()
     {
         var ray = controlledCamera.ScreenPointToRay(Input.mousePosition);
